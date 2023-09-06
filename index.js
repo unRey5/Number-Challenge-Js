98d7e37c2b9d53273b309b858b9bb8989e93909d
Daniel Odey <dannyandcode@gmail.com>
2023-09-06 03:46:27 -0400

    adding Js practice files

---
+++ index.js
@@ -0,0 +1,25 @@
+//number challenge
+
+let x = Math.round(Math.random() * 100 + 1);
+
+let y = Math.round(Math.random() * 50 + 1);
+
+let sum = `${x} + ${y} = ${x + y}`; 
+
+let difference = `${x} - ${y} = ${x - y}`;
+
+let product = `${x} * ${y} = ${x * y}`;
+
+let quotient = `${x} / ${y} = ${x / y}`;
+
+let remainder = `${x} % ${y} = ${x % y}`;
+
+console.log(remainder);
+
+console.log(quotient);
+
+console.log(product);
+
+console.log(difference);
+
+console.log(sum);
